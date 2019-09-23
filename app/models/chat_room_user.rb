e0244918c48108a3187b8df19e58630d8e858b23
hayatosann <sasano.hayato@icloud.com>
2019-09-23 15:14:14 +0900

    Write down set column

--- app/models/chat_room_user.rb
+++ app/models/chat_room_user.rb
@@ -1,2 +1,9 @@
 class ChatRoomUser < ApplicationRecord
+  def change
+    create_table :chat_room_users do |t|
+      t.integer :chat_room_id, null: false
+      t.integer :user_id, null: false
+      t.timestamps
+    end
+  end
 end
