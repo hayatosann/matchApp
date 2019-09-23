6267b053909b91ea47f0c2b0df3f3b3853d93f58
hayatosann <sasano.hayato@icloud.com>
2019-09-23 16:14:27 +0900

    Make table

--- app/models/chat_room_user.rb
+++ app/models/chat_room_user.rb
@@ -1,9 +1,4 @@
 class ChatRoomUser < ApplicationRecord
-  def change
-    create_table :chat_room_users do |t|
-      t.integer :chat_room_id, null: false
-      t.integer :user_id, null: false
-      t.timestamps
-    end
-  end
+  belongs_to :chat_room
+  belongs_to :user
 end
