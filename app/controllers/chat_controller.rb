4efc66e3cbd396fc5a10df4d7799921d31f703f6
hayatosann <sasano.hayato@icloud.com>
2019-09-23 18:49:43 +0900

    modify sentence

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -18,8 +18,7 @@ class ChatController < ApplicationController
   def show
     # チャット相手の情報を取得する
     chat_room = ChatRoom.find_by(id: params[:id])
-    @chat_room_user = chat_room.chat_room_users.
-      where.not(user_id: current_user.id).first.user
+    @chat_room_user = chat_room.chat_room_users.where.not(user_id: current_user.id).first.user
     @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
   end
 end
