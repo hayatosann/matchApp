7b6756978dadd5e02ea3dea109002b4f06cdd901
hayatosann <sasano.hayato@icloud.com>
2019-09-24 19:54:22 +0900

    modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.11.1'
 
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 set :application, "matchApp"
 
 set :repo_url,  'git@github.com:hayatosann/matchApp.git'
