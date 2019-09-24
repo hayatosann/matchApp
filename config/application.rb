ccf53507528aaa7fce8ca77a53836c0a0756d452
hayatosann <sasano.hayato@icloud.com>
2019-09-25 01:46:20 +0900

    modify

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.paths.add 'lib', eager_load: true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +10,7 @@ module MatchApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+    config.paths.add 'lib', eager_load: true
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
